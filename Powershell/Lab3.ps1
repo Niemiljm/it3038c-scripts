28bb5244d482ebc49ea08393b7206ba8316f8050
Niemiljm <niemiljm@mail.uc.edu>
2021-02-02 18:56:30 -0500

    IT3038c

---
+++ Powershell/Lab3.ps1
@@ -0,0 +1,10 @@
+﻿
+function getIP{
+    (get-netipaddress).ipv4address | Select-String "192*"
+    }
+
+$IP = getIP
+$NAME= Get-Content env:COMPUTERNAME
+$Pow = Get-Host | Select-Object Version
+$BODY= "This machine's IP is: $IP The User is an Administrator. The hostname is: ($env:COMPUTERNAME) "
+Send-MailMessage -To "joester4life97@gmail.com" -From "niemiljm@mail.uc.edu" -Subject "IT3038C Windows SysInfo Test Information" -Body $BODY -SmtpServer smtp.google.com -port 587 -UseSSL -Credential (Get-Credential)
