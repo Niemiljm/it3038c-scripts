94ee4fc0ccb4bad14a5dfc91cb882784e17f8a3f
Niemiljm <niemiljm@mail.uc.edu>
2021-04-17 18:03:33 -0400

    Project

--- Project2/README.txt
+++ Project2/README.txt
@@ -1,4 +1,75 @@
-Packages: 
-pip install scapy
-pip install argparse    https://docs.python.org/3.4/library/subprocess.html
-pip install re
+
+--------------------------------
+|        My System Info        |        
+--------------------------------
+
+My System info is a python script that goes through some hardware and networking information on your computer.
+It uses a variety of packages, some of which need to be installed in order to function, but I'll get into that later. 
+
+The things my script can do:
+1. Spit out a full list of installed programs on your device.
+2. Give you a total amount of RAM on the system, as well as the percentage that is being used. 
+3. Display your Hostname, Public IP address, ad well as your local IP address.
+4. Names the processor in the client, and also gives the number of cores and their percent usage.
+5. Does all of the above in one go as an added option.
+
+--------------------------------
+|          Motivation          |        
+--------------------------------
+
+Seeing the type of programs that we were using in class, I figured this could be something that I could use on a daily basis at work.
+Since I work helpdesk, there has sometimes been a need to get a full list of programs on someone's computer, and without knowing cmd
+there is no "simple" way to do this. Although there are other ways to do it, I wanted to make my own!
+
+I also wanted to learn how to take user inputs and use them to execute functions. Normal programs can do that, why not mine?
+
+The other functions were added because I wanted to give myself a challenge and a wide range of experience with external and internal python packages.
+
+
+--------------------------------
+|         Installation         |        
+--------------------------------
+
+Things you'll need:
+1. Python 3.6+ Installed
+2. An IDE (I used Visual Studio Code)
+3. The following packages:              (If any are not installed for some reason, use the commands below)
+
+subprocess                              pip install subprocess
+socket                                  pip install socket
+requests                                pip install requests
+psutil                                  pip install psutil
+platform                                pip install platform
+from psutil import virtual_memory
+from subprocess import check_output
+
+
+-------------------------------------------------------------------------------------------------------
+
+Time to actually get some work done.
+1. Open up your IDE and change directory to your working directory for the project.
+2. In the working directory terminal, install the packages listed above with the corresponding pip install commands.
+3. Run the program by typing "python mySysInfo.py" in the working directory terminal without the quotations. 
+
+If all is well and configured properly, you should be able to run the code.
+An example of the output that you should see is shown here: ()
+
+
+--------------------------------
+|          References          |        
+--------------------------------
+I would be lying if I said I did this completely on my own and without guidance from other sources.
+
+1. I used this to learn how to run functions based on input: https://stackoverflow.com/questions/48502404/user-input-to-call-function
+2. I used this in order to get the function to retrieve a list of programs on the device: https://www.geeksforgeeks.org/get-a-list-of-installed-softwares-in-windows-using-python/
+3. I spent more time than I would like to admit here: https://psutil.readthedocs.io/en/latest/
+4. This link taught me how to properly write a README: https://meakaakka.medium.com/a-beginners-guide-to-writing-a-kickass-readme-7ac01da88ab3
+5. Also: https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#headers
+
+
+
+
+
+
+
+
--- Project2/mySysInfo.py
+++ Project2/mySysInfo.py
@@ -1,3 +1,134 @@
-import scapy
-import argparse
-import re
+import subprocess
+import socket
+import requests
+import psutil
+import platform
+from psutil import virtual_memory
+from subprocess import check_output
+
+
+
+class the_action(object):
+
+    # ProgramList will spit out a program list | IP will display the device ipconfig | Memory will display the total memory and free memory | CPU will display the CPU info | All displays all of these.
+    def ProgramList(run):
+        print("One moment while I grab that list for you!")
+        print("-------------------------------------------------------------")
+        ProgramList = subprocess.check_output(['wmic', 'product', 'get', 'name'])
+        plist = str(ProgramList)
+
+        try:
+            for a in range(len(plist)):
+                print(plist.split("\\r\\r\\n")[6:][a])
+
+        except IndexError as e:
+            print("-------------------------------------------------------------")
+            print("|                   Your list is above!                     |")
+            print("-------------------------------------------------------------")
+            run.__init__()
+
+    # Memory will give a total amount of RAM in GB as well as the current % usage of RAM.
+    def Memory(run):
+        ram = virtual_memory()
+        ramTot = ram.total/(1024.**3)
+        ramPer = ram.percent
+        print("-------------------------------------------------------------")
+        print(ramTot, "GB of RAM")
+        print(ramPer, "percentage used currently")
+        print("-------------------------------------------------------------")
+        run.__init__()
+
+    # IPList will give you a few bits of information about your networking
+    def IPList(run):
+        hostname = socket.gethostname()
+        localIP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        localIP.connect(("8.8.8.8", 80))                                        # This opens a socket to google, to ensure that the NIC we're testing can connect to the internet. Otherwise you may get a 127 address, which we don't want.
+        publicIP = requests.get("http://wtfismyip.com/text").text               # This reaches out to wtfismyip to get your public facing IP address
+        print("-------------------------------------------------------------")
+        print(hostname, " Is your hostname")                                    # This simply gets your hostname, since you can get your localIP from the hostname
+        print(publicIP, " Is your public IP address")
+        print(localIP.getsockname()[0], " Is your local IP address")
+        print("-------------------------------------------------------------")
+        localIP.close()                                                         # This closes the socket so that is doesn't keep running.
+        run.__init__()
+
+    # CPUList is the function that will output the name/type of processor your device has, as well as the percentage of usage in each core.
+    def CPUList(run):
+        for x in range(3):
+            CpuUsage = psutil.cpu_percent(interval=1, percpu=True)              # Goes through each core and pulls percentages
+        CpuName = platform.processor()                                          # Gets the name of your cpu "Intel64 Family 6 Model 158 Stepping 9, GenuineIntel"
+        print("This one can take a little bit of time, don't panic!")
+        print("-------------------------------------------------------------")
+        print(CpuName, " Is the name of your processor")
+        print(CpuUsage, " Is the percentage usage of each CPU Core")
+        print("-------------------------------------------------------------")
+        run.__init__()
+
+    # AllRun runs everything above in one go. 
+    def AllRun(run):
+        print("Hang tight, this one might take a second!")          
+        ProgramList = subprocess.check_output(['wmic', 'product', 'get', 'name'])   # This is the command to get program list from cmd
+        plist = str(ProgramList)
+        try:
+            for a in range(len(plist)):
+                print(plist.split("\\r\\r\\n")[6:][a])                              # Formatting the list into something readable
+        except IndexError as e:                                                     # When it fails, stop.
+            print("-------------------------------------------------------------")
+            print("|               Your program list is above!                 |")
+            print("-------------------------------------------------------------")
+
+        # Memory will give a total amount of RAM in GB as well as the current % usage of RAM.
+        ram = virtual_memory()
+        ramTot = ram.total/(1024.**3)                                           # Makes the RAM total readable by converting it to GB
+        ramPer = ram.percent                                                    # Gives current usage data for RAM
+        print("-------------------------------------------------------------")
+        print(ramTot, "GB of RAM")
+        print(ramPer, "percentage used currently")       
+        print("-------------------------------------------------------------")
+
+        # IPList will give you a few bits of information about your networking
+        hostname = socket.gethostname()
+        localIP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        localIP.connect(("8.8.8.8", 80))                                        # This opens a socket to google, to ensure that the NIC we're testing can connect to the internet. Otherwise you may get a 127 address, which we don't want.
+        publicIP = requests.get("http://wtfismyip.com/text").text               # This reaches out to wtfismyip to get your public facing IP address
+        print("-------------------------------------------------------------")
+        print(hostname, " Is your hostname")                                    # This simply gets your hostname, since you can get your localIP from the hostname
+        print(publicIP, " Is your public IP address")
+        print(localIP.getsockname()[0], " Is your local IP address")
+        localIP.close()
+        print("-------------------------------------------------------------")
+
+        # CPUList is the function that will output the name/type of processor your device has, as well as the percentage of usage in each core.
+        for x in range(3):
+            CpuUsage = psutil.cpu_percent(interval=1, percpu=True)              # Goes through each core and pulls percentages
+        CpuName = platform.processor()                                          # Gets the name of your cpu "Intel64 Family 6 Model 158 Stepping 9, GenuineIntel"
+        print("-------------------------------------------------------------")
+        print(CpuName, " Is the name of your processor")
+        print(CpuUsage, " Is the percentage usage of each CPU Core")
+        print("-------------------------------------------------------------")
+        print("Phew!! That was a lot of work!")
+        run.__init__()
+
+    #This takes the input from the user
+    def __init__(run):
+        run.init = input("What would you like to do? (1 = Program, 2 = Memory, 3 = IP, 4 = CPU, 5 = All, 6 = Exit) Please enter a number from the list: ")
+        list = [1, 2, 3, 4, 5, 6]
+
+        if run.init == str(1):                                                  # This if statement takes the init input and determines which functions to run (ProgramList, IPList, CPUList, or AllRun)           
+            run.ProgramList()
+        elif run.init == str(2):
+            run.Memory()
+        elif run.init == str(3):
+            run.IPList()
+        elif run.init == str(4):
+            run.CPUList()
+        elif run.init == str(5):
+            run.AllRun()
+        elif run.init == str(6):
+            print("Thank you come again!")
+            exit(0)       
+        elif run.init != list:
+            print("Please submit a proper input: ") # In the event you try to get smart and submit a letter or invalid character, it will fail and return until you do it right.
+            run.__init__()
+        
+a = the_action(); 
