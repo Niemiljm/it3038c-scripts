77ab5fb3a5034558534575bbccc6ae2ec70d159b
Niemiljm <niemiljm@mail.uc.edu>
2021-04-20 12:58:05 -0400

    p3

--- Project3/wtwbbot.py
+++ Project3/wtwbbot.py
@@ -1,5 +1,7 @@
 # Please be aware that if you decide to use the subreddits below, some posts will be NSFW.
 # Browse at your own risk. This bot was made for education rather than the content of those subreddits.
+# If you would like to see the bot in action, please browse to https://www.reddit.com/r/WtWBBot/ 
+
 
 import re
 import requests
